f1187f115242d48e4c8fd4aa1fc3d41e69b2e123
bonukai <17750886+bonukai@users.noreply.github.com>
2022-01-20 12:35:18 +0000

    Implement changing password page

--- client/src/pages/settings/Password.tsx
+++ client/src/pages/settings/Password.tsx
@@ -1,25 +1,76 @@
-import React from 'react';
+import React, { useState } from 'react';
+import clsx from 'clsx';
+
+import { FetchError } from 'src/api/api';
 import { useUser } from 'src/api/user';
 
 export const SettingsPasswordPage = () => {
   const { updatePassword } = useUser();
+  const [currentPassword, setCurrentPassword] = useState('');
+  const [newPassword, setNewPassword] = useState('');
+  const [confirmNewPassword, setConfirmNewPassword] = useState('');
+
+  const [success, setSuccess] = useState<boolean>();
+  const [message, setMessage] = useState<string>();
 
   return (
-    <form onSubmit={(e) => e.preventDefault()}>
-      <div>Current password</div>
-      <input type="password" />
+    <>
+      <form onSubmit={(e) => e.preventDefault()}>
+        <div>Current password</div>
+        <input
+          type="password"
+          value={currentPassword}
+          onChange={(e) => setCurrentPassword(e.currentTarget.value)}
+        />
 
-      <div>New password</div>
-      <input type="password" />
-      <div>Confirm new password</div>
-      <input type="password" />
-      <br />
-      <button
-        className="mt-2 btn"
-        onClick={() => updatePassword({ currentPassword: '', newPassword: '' })}
-      >
-        Change password
-      </button>
-    </form>
+        <div>New password</div>
+        <input
+          type="password"
+          value={newPassword}
+          onChange={(e) => setNewPassword(e.currentTarget.value)}
+        />
+        <div>Confirm new password</div>
+        <input
+          type="password"
+          value={confirmNewPassword}
+          onChange={(e) => setConfirmNewPassword(e.currentTarget.value)}
+        />
+        <br />
+        <button
+          className="mt-2 btn"
+          onClick={async () => {
+            if (newPassword !== confirmNewPassword) {
+              setSuccess(false);
+              setMessage('Passwords do not match');
+              return;
+            }
+
+            try {
+              await updatePassword({
+                currentPassword: currentPassword,
+                newPassword: newPassword,
+              });
+
+              setSuccess(true);
+              setMessage('Password has been changed');
+            } catch (error) {
+              if (error instanceof FetchError && error.status === 401) {
+                setSuccess(false);
+                setMessage('Current password is incorrect');
+              }
+            }
+          }}
+        >
+          Change password
+        </button>
+      </form>
+      {success !== undefined && (
+        <div
+          className={clsx('mt-2', success ? 'text-green-500' : 'text-red-500')}
+        >
+          {message}
+        </div>
+      )}
+    </>
   );
 };
