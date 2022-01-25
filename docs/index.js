c37a09c48ec3762002b967fff3c688fb70677b45
bonukai <17750886+bonukai@users.noreply.github.com>
2022-01-25 23:33:11 +0000

    Fix dosc

--- docs/index.js
+++ docs/index.js
@@ -1,7 +1,7 @@
 import SwaggerUI from 'swagger-ui';
 import 'swagger-ui/dist/swagger-ui.css';
 
-const spec = require('./../openapi.json');
+const spec = require('../server/openapi.json');
 
 const ui = SwaggerUI({
     spec: spec,
