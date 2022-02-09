ecba107cc5df864ae70564a54863266ac879e167
bonukai <17750886+bonukai@users.noreply.github.com>
2022-02-09 17:23:58 +0000

    Do not display "Running migrations", when is up to date

--- server/src/dbconfig.ts
+++ server/src/dbconfig.ts
@@ -4,17 +4,15 @@ import { t, plural } from '@lingui/macro';
 import config, { migrationsDirectory } from './knexfile';
 
 export const runMigrations = async () => {
-    console.log(t`Running migrations`);
-
     const [batchNo, log] = await knex.migrate.latest({
         directory: migrationsDirectory,
     });
 
-    if (log.length === 0) {
-        console.log(t`Already up to date`);
-    } else {
+    if (log.length > 0) {
         const name = batchNo;
         const count = log.length;
+
+        console.log(t`Running migrations`);
 
         console.log(
             t`Batch ${name} run: ${plural(count, {
