7a3d78aa2e284edd5dd38280ca7cebf2d966f71e
bonukai <17750886+bonukai@users.noreply.github.com>
2022-02-11 00:45:50 +0000

    Move btn class to tailwindcss config file

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,6 +1,48 @@
+/** @type {import("@types/tailwindcss/plugin").TailwindPluginFn } */
+const plugin = require('tailwindcss/plugin');
+
 /** @type {import("@types/tailwindcss/tailwind-config").TailwindConfig } */
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: 'class',
   important: true,
+  plugins: [
+    plugin(({ addUtilities }) => {
+      addUtilities({
+        '.writing-mode-vertical': {
+          'writing-mode': 'vertical-lr',
+        },
+
+        '.writing-mode-initial': {
+          'writing-mode': 'initial',
+        },
+      });
+    }),
+    plugin(({ addUtilities }) => {
+      const btn = {
+        display: 'inline-block',
+        padding: '2px 16px',
+        borderRadius: '4px',
+        borderWidth: '1px',
+        cursor: 'pointer',
+        transition: 'all 150ms',
+        userSelect: 'none',
+        padding: '2px 16px',
+        '@apply dark:border-current border-current hover:shadow shadow-current hover:shadow-current disabled:shadow-none disabled:cursor-auto dark:disabled:bg-gray-500 disabled:bg-gray-300':
+          {},
+      };
+
+      addUtilities({
+        '.btn': btn,
+        '.btn-red': {
+          ...btn,
+          '@apply text-red-700 dark:text-red-500': {},
+        },
+        '.btn-blue': {
+          ...btn,
+          '@apply text-blue-800 dark:text-blue-300': {},
+        },
+      });
+    }),
+  ],
 };
