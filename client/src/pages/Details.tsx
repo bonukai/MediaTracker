4151dedf023d4490743e0e283aa73f8b8f95cc87
BanCrash <BanCrash@users.noreply.github.com>
2022-02-22 23:47:46 +0100

    Added missing translation (#186)

--- client/src/pages/Details.tsx
+++ client/src/pages/Details.tsx
@@ -645,7 +645,7 @@ const SetProgressButton: FunctionComponent<{
     <Modal
       openModal={(openModal) => (
         <div className="text-sm text-green-500 btn" onClick={() => openModal()}>
-          Set progress
+          <Trans>Set progress</Trans>
         </div>
       )}
     >
