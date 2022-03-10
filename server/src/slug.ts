28a3b298875464846127f647f7fce322b8e863f4
bonukai <17750886+bonukai@users.noreply.github.com>
2022-03-10 21:44:06 +0000

    update slug function

--- server/src/slug.ts
+++ server/src/slug.ts
@@ -1,13 +1,15 @@
 import { customAlphabet } from 'nanoid';
 
 export const toSlug = (str: string) => {
-  return str
-    .toLowerCase()
-    .replaceAll(/[^a-z0-9\s-]/g, '')
-    .replaceAll(/\s+/g, '-')
-    .replaceAll(/-+/g, '-')
-    .replaceAll(/(?<!^)-$/g, '')
-    .replaceAll(/^-(?!$)/g, '');
+  return (
+    str
+      .toLowerCase()
+      .replaceAll(/[^a-z0-9\s-]/g, '')
+      .replaceAll(/\s+/g, '-')
+      .replaceAll(/-+/g, '-')
+      .replaceAll(/-$/g, '')
+      .replaceAll(/^-/g, '') || '-'
+  );
 };
 
 export const randomSlugId = customAlphabet(
