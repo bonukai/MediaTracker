fe5b4bb0628be9e0bef2441c893ea25834b504c9
bonukai <bonukai@protonmail.com>
2022-03-12 18:51:02 +0000

    activate detected language only if translations are available (#228)

--- client/src/i18n/i18n.ts
+++ client/src/i18n/i18n.ts
@@ -8,11 +8,24 @@ import { messages as en } from 'src/i18n/locales/en/translation';
 import { messages as es } from 'src/i18n/locales/es/translation';
 
 export const setupI18n = () => {
-  const locale = detect(fromNavigator(), 'en').split('-')?.at(0) || 'en';
+  const allMessages = {
+    da: da,
+    de: de,
+    en: en,
+    es: es,
+  };
+
+  const supportedLanguages = Object.keys(allMessages);
+
+  const detectedLocale = detect(fromNavigator(), 'en').split('-')?.at(0);
+
+  const locale = supportedLanguages.includes(detectedLocale)
+    ? detectedLocale
+    : 'en';
 
   i18n.loadLocaleData({
     [locale]: { plurals: plurals[locale] },
   });
-  i18n.load({ da: da, de: de, en: en, es: es });
+  i18n.load(allMessages);
   i18n.activate(locale);
 };
