c3a685c58c929e5e0508c48114c6cc567d68ec35
bonukai <17750886+bonukai@users.noreply.github.com>
2022-05-04 14:05:10 +0100

    add lingui to Confirm popup

--- client/src/components/Confirm.tsx
+++ client/src/components/Confirm.tsx
@@ -1,6 +1,8 @@
 import React, { FunctionComponent } from 'react';
 import ReactDOM from 'react-dom';
 import { Trans } from '@lingui/macro';
+import { I18nProvider } from '@lingui/react';
+import { i18n } from '@lingui/core';
 
 import { Modal } from 'src/components/Modal';
 
@@ -12,19 +14,23 @@ export const Confirm = async (message: string) => {
 
   return await new Promise<boolean>((resolve) => {
     ReactDOM.render(
-      <Modal>
-        {(closeModal) => (
-          <ModalBody
-            message={message}
-            resolve={(res) => {
-              closeModal();
-              portal.removeChild(node);
+      <React.StrictMode>
+        <I18nProvider i18n={i18n}>
+          <Modal>
+            {(closeModal) => (
+              <ModalBody
+                message={message}
+                resolve={(res) => {
+                  closeModal();
+                  portal.removeChild(node);
 
-              resolve(res);
-            }}
-          />
-        )}
-      </Modal>,
+                  resolve(res);
+                }}
+              />
+            )}
+          </Modal>
+        </I18nProvider>
+      </React.StrictMode>,
       node
     );
   });
