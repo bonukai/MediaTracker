fa7d1a2493aaf2920273db170d6294af67d4b405
Jon Kristian Nilsen <jon@divint.no>
2023-05-08 18:07:09 +0200

    Adds discord webhook notification.

---
+++ server/src/notifications/platforms/discord.ts
@@ -0,0 +1,22 @@
+import axios from 'axios';
+import { createNotificationPlatform } from 'src/notifications/createNotificationPlatform';
+
+export const Discord = createNotificationPlatform({
+  name: 'Discord',
+  credentialNames: <const>['url'],
+  sendFunction: async (args) => {
+    const { message, title, credentials } = args;
+
+    await axios.post(
+      new URL(credentials.url).href,
+      {
+        content: message
+      },
+      {
+        headers: {
+          'Content-Type': "application/json",
+        },
+      }
+    );
+  },
+});
