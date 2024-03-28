982084007bb2adc7e9e0288a7da16600f9651576
bonukai <17750886+bonukai@users.noreply.github.com>
2024-03-28 22:31:52 +0000

    use env variables in CLI in Dockerfile

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -8,13 +8,27 @@ if ! [ "$PUID" -eq "$(id -u abc)" ]; then
     usermod --non-unique --uid "$PUID" abc
 fi
 
-echo 
+echo
 echo PUID: $(id -u abc)
 echo PGID: $(id -g abc)
-echo 
+echo
 
 chown -R abc:abc /storage
 chown -R abc:abc /assets
 chown -R abc:abc /logs
 
-su -c "node /app/build/index.js start-server --db-filepath /storage/data.db --logs-dir /logs --assets-dir /assets" abc
+args="--db-client SQLite "
+
+if [ -n "${DATABASE_CLIENT}" ]; then
+    args="${args} --db-client ${DATABASE_CLIENT}"
+else
+    args="${args} --db-client SQLite"
+fi
+
+if [ -n "${DATABASE_CONNECTION_STRING}" ]; then
+    args="${args} --db-connection-string ${DATABASE_CONNECTION_STRING}"
+else
+    args="${args} --db-filepath /storage/data.db"
+fi
+
+su -c "node /app/build/index.js start-server --logs-dir /logs --assets-dir /assets ${args}" abc
