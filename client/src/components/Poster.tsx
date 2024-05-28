575a84c6cdbec35a89a12535beaaa37989c7a58a
bonukai <17750886+bonukai@users.noreply.github.com>
2024-05-28 14:55:58 +0100

    Fix browser "freeze" when downloading new images

--- client/src/components/Poster.tsx
+++ client/src/components/Poster.tsx
@@ -2,6 +2,7 @@ import type { MediaItemResponse } from '@server/entity/mediaItemModel';
 import { FC, useEffect, useRef, useState } from 'react';
 import { cx } from '../utils';
 import { posterAspectRatio, posterSrc } from '../mediaItemHelpers';
+import { useImg } from '../hooks/useImg';
 
 export const Poster: FC<{
   mediaItem: MediaItemResponse;
@@ -11,22 +12,11 @@ export const Poster: FC<{
 }> = (props) => {
   const { mediaItem, width, className } = props;
   const roundedCorners = props.roundedCorners || 'all';
-  const src = posterSrc(mediaItem, width);
-  const [imageLoaded, setImageLoaded] = useState(false);
-  const imageCachedRef = useRef<boolean | null>(null);
   const imgRef = useRef<HTMLImageElement>(new Image());
   const elementRef = useRef<HTMLDivElement>(null);
   const [fontSize, setFontSize] = useState<number>();
 
-  if (src) {
-    imgRef.current.src = src;
-
-    if (imageCachedRef.current === null && imgRef.current.naturalHeight > 0) {
-      imageCachedRef.current = true;
-    } else {
-      imageCachedRef.current = false;
-    }
-  }
+  const { src, loading } = useImg(posterSrc(mediaItem, width));
 
   useEffect(() => {
     if (!elementRef.current) {
@@ -45,54 +35,11 @@ export const Poster: FC<{
         className
       )}
     >
-      {/* <div
-        className={cx(
-          'absolute top-0 left-0 flex flex-row items-center justify-center w-full h-full antialiased text-center text-red-700 align-middle  bg-orange-200 select-none overflow-clip',
-          roundedCorners === 'all'
-            ? 'rounded'
-            : roundedCorners === 'top'
-            ? 'rounded-t'
-            : roundedCorners === 'left'
-            ? 'rounded-t'
-            : '',
-          imageCachedRef.current === false &&
-            (imageLoaded ? 'opacity-0' : 'opacity-100')
-        )}
-        style={{
-          fontSize: fontSize ? `${fontSize}px` : '',
-        }}
-      >
-        ?
-      </div>
-
-      {src && (
+      {!loading && src ? (
         <img
           src={src}
           ref={imgRef}
           draggable="false"
-          onLoad={() => setImageLoaded(true)}
-          className={cx(
-            'antialiased absolute top-0 left-0 w-full h-full shadow-md object-fill overflow-clip z-10',
-            roundedCorners === 'all'
-              ? 'rounded'
-              : roundedCorners === 'top'
-              ? 'rounded-t'
-              : roundedCorners === 'left'
-              ? 'rounded-l'
-              : '',
-            imageCachedRef.current === false &&
-              (imageLoaded ? 'opacity-100' : 'opacity-0 blur-xl'),
-            className
-          )}
-        />
-      )} */}
-
-      {src ? (
-        <img
-          src={src}
-          ref={imgRef}
-          draggable="false"
-          onLoad={() => setImageLoaded(true)}
           className={cx(
             'antialiased absolute top-0 left-0 w-full h-full shadow-md object-fill overflow-clip z-10',
             roundedCorners === 'all'
@@ -102,15 +49,16 @@ export const Poster: FC<{
                 : roundedCorners === 'left'
                   ? 'rounded-l'
                   : '',
-            imageCachedRef.current === false &&
-              (imageLoaded ? 'opacity-100' : 'opacity-0 blur-xl'),
+
+            !loading ? 'opacity-100' : 'opacity-0 blur-xl',
             className
           )}
         />
       ) : (
         <div
           className={cx(
-            'absolute top-0 left-0 flex flex-row items-center justify-center w-full h-full antialiased text-center text-red-700 align-middle  bg-orange-200 select-none overflow-clip',
+            'absolute top-0 left-0 flex flex-row items-center justify-center w-full h-full antialiased text-center  align-middle  select-none overflow-clip',
+            loading && 'text-red-700 bg-orange-200',
             roundedCorners === 'all'
               ? 'rounded'
               : roundedCorners === 'top'
@@ -118,14 +66,14 @@ export const Poster: FC<{
                 : roundedCorners === 'left'
                   ? 'rounded-t'
                   : '',
-            imageCachedRef.current === false &&
-              (imageLoaded ? 'opacity-0' : 'opacity-100')
+
+            !loading ? 'opacity-0' : 'opacity-100'
           )}
           style={{
             fontSize: fontSize ? `${fontSize}px` : '',
           }}
         >
-          ?
+          {loading && <>?</>}
         </div>
       )}
     </div>
---
+++ client/src/hooks/useImg.ts
@@ -0,0 +1,50 @@
+import { useEffect, useState } from 'react';
+
+export const useImg = (src?: string | null) => {
+  const [hasImage, setHasImage] = useState<boolean>();
+
+  useEffect(() => {
+    if (!src) {
+      return;
+    }
+
+    let timeoutId: NodeJS.Timeout;
+
+    const maxAttempts = 5;
+    const fetchImage = async (attempt: number, retryIn: number) => {
+      if (attempt > maxAttempts) {
+        return;
+      }
+
+      const res = await fetch(src);
+
+      if (res.ok) {
+        const img = new Image();
+        img.src = src;
+        img.onload = () => {
+          setHasImage(true);
+        };
+      } else {
+        setHasImage(false);
+
+        timeoutId = setTimeout(async () => {
+          await fetchImage(attempt + 1, retryIn * 4);
+        }, retryIn);
+      }
+    };
+
+    fetchImage(1, 100);
+
+    return () => {
+      if (timeoutId !== undefined) {
+        clearTimeout(timeoutId);
+      }
+    };
+  }, []);
+
+  return {
+    hasImage: typeof src === 'string',
+    src: hasImage ? src : undefined,
+    loading: hasImage === undefined ? false : !hasImage,
+  };
+};
