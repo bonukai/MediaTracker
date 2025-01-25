0722017cafb0fe5bcd099e0097ae79b2ed212366
bonukai <17750886+bonukai@users.noreply.github.com>
2025-01-25 23:32:49 +0000

    check for upcoming airing only for the next 24 hours

--- src/lastAndUpcomingAirings.ts
+++ src/lastAndUpcomingAirings.ts
@@ -1,3 +1,4 @@
+import { addDays } from 'date-fns';
 import { Database } from './database.js';
 import { logger } from './logger.js';
 import { mediaItemRepository } from './repository/mediaItemRepository.js';
@@ -5,107 +6,133 @@ import { scheduleJob } from './scheduler.js';
 import { h } from './utils.js';
 
 export const scheduleLastAndUpcomingAiringsUpdate = async () => {
-  const currentDateString = new Date().toISOString();
+  const now = new Date();
+  const endDate = addDays(now, 1);
 
-  await Database.knex.transaction(async (trx) => {
-    const nextEpisodeReleaseDate = await trx('episode')
-      .min({
-        releaseDate: 'releaseDate',
-      })
-      .where('releaseDate', '>', currentDateString)
-      .first();
+  const currentDateString = now.toISOString();
+  const endDateString = endDate.toISOString();
 
-    if (!nextEpisodeReleaseDate || !nextEpisodeReleaseDate.releaseDate) {
-      return;
-    }
+  const getEpisodesToUpdate = () => {
+    return Database.knex.transaction(async (trx) => {
+      const nextEpisodeReleaseDate = await trx('episode')
+        .min({
+          releaseDate: 'releaseDate',
+        })
+        .where('releaseDate', '>', currentDateString)
+        .where('releaseDate', '<=', endDateString)
+        .first();
 
-    const date = new Date(nextEpisodeReleaseDate.releaseDate);
+      if (!nextEpisodeReleaseDate || !nextEpisodeReleaseDate.releaseDate) {
+        return;
+      }
 
-    if (isNaN(date.getTime())) {
-      return;
-    }
+      const date = new Date(nextEpisodeReleaseDate.releaseDate);
 
-    const upcomingEpisodes = await trx('episode').where(
-      'releaseDate',
-      nextEpisodeReleaseDate.releaseDate
-    );
+      if (isNaN(date.getTime())) {
+        return;
+      }
 
-    const mediaItemsToUpdate = upcomingEpisodes.map(
-      (episode) => episode.tvShowId
-    );
+      const upcomingEpisodes = await trx('episode').where(
+        'releaseDate',
+        nextEpisodeReleaseDate.releaseDate
+      );
 
-    scheduleJob({
-      name: 'update next and upcoming episodes',
-      date,
-      job: async () => {
-        if (mediaItemsToUpdate.length === 0) {
-          logger.debug(h`nothing to update`);
-        } else {
-          await Database.knex.transaction((trx) =>
-            mediaItemRepository.updateLastAndUpcomingEpisodeAirings({
-              mediaItemIds: mediaItemsToUpdate,
-              trx,
-            })
-          );
+      const mediaItemsToUpdate = upcomingEpisodes.map(
+        (episode) => episode.tvShowId
+      );
 
-          logger.debug(
-            h`updated last and upcoming episodes for ${mediaItemsToUpdate.length} shows: ${mediaItemsToUpdate.join(', ')}`
-          );
-        }
+      if (mediaItemsToUpdate.length === 0) {
+        return;
+      }
 
-        await scheduleLastAndUpcomingAiringsUpdate();
-      },
+      return { mediaItemsToUpdate, date };
     });
+  };
+
+  const episodesToUpdate = await getEpisodesToUpdate();
+
+  scheduleJob({
+    name: 'update next and upcoming episodes',
+    date: episodesToUpdate ? episodesToUpdate.date : endDate,
+    job: async () => {
+      if (episodesToUpdate) {
+        const { mediaItemsToUpdate } = episodesToUpdate;
+
+        await Database.knex.transaction((trx) =>
+          mediaItemRepository.updateLastAndUpcomingEpisodeAirings({
+            mediaItemIds: mediaItemsToUpdate,
+            trx,
+          })
+        );
+
+        logger.debug(
+          h`updated last and upcoming episodes for ${mediaItemsToUpdate.length} shows: ${mediaItemsToUpdate.join(', ')}`
+        );
+      }
+
+      await scheduleLastAndUpcomingAiringsUpdate();
+    },
   });
 
-  await Database.knex.transaction(async (trx) => {
-    const nextMediaItemReleaseDate = await trx('mediaItem')
-      .min({
-        releaseDate: 'releaseDate',
-      })
-      .where('releaseDate', '>', currentDateString)
-      .first();
+  const getMediaItemsToUpdate = () => {
+    return Database.knex.transaction(async (trx) => {
+      const nextMediaItemReleaseDate = await trx('mediaItem')
+        .min({
+          releaseDate: 'releaseDate',
+        })
+        .where('releaseDate', '>', currentDateString)
+        .where('releaseDate', '<=', endDateString)
+        .first();
 
-    if (!nextMediaItemReleaseDate || !nextMediaItemReleaseDate.releaseDate) {
-      return;
-    }
+      if (!nextMediaItemReleaseDate || !nextMediaItemReleaseDate.releaseDate) {
+        return;
+      }
 
-    const date = new Date(nextMediaItemReleaseDate.releaseDate);
+      const date = new Date(nextMediaItemReleaseDate.releaseDate);
 
-    if (isNaN(date.getTime())) {
-      return;
-    }
+      if (isNaN(date.getTime())) {
+        return;
+      }
 
-    const upcomingMediaItems = await trx('mediaItem').where(
-      'releaseDate',
-      nextMediaItemReleaseDate.releaseDate
-    );
+      const upcomingMediaItems = await trx('mediaItem').where(
+        'releaseDate',
+        nextMediaItemReleaseDate.releaseDate
+      );
 
-    const mediaItemsToUpdate = upcomingMediaItems.map(
-      (mediaItem) => mediaItem.id
-    );
+      const mediaItemsToUpdate = upcomingMediaItems.map(
+        (mediaItem) => mediaItem.id
+      );
 
-    scheduleJob({
-      name: 'update next and upcoming media items',
-      date,
-      job: async () => {
-        if (mediaItemsToUpdate.length === 0) {
-          logger.debug(h`nothing to update`);
-        } else {
-          await Database.knex.transaction((trx) =>
-            mediaItemRepository.updateLastAndNextMediaItemAirings({
-              mediaItemIds: mediaItemsToUpdate,
-              trx,
-            })
-          );
+      if (mediaItemsToUpdate.length === 0) {
+        return;
+      }
 
-          logger.debug(
-            h`updated last and upcoming dates for ${mediaItemsToUpdate.length} mediaItems: ${mediaItemsToUpdate.join(', ')}`
-          );
-        }
-
-        await scheduleLastAndUpcomingAiringsUpdate();
-      },
+      return { mediaItemsToUpdate, date };
     });
+  };
+
+  const nonTvItemsToUpdate = await getMediaItemsToUpdate();
+
+  scheduleJob({
+    name: 'update next and upcoming media items',
+    date: nonTvItemsToUpdate ? nonTvItemsToUpdate.date : endDate,
+    job: async () => {
+      if (nonTvItemsToUpdate) {
+        const { mediaItemsToUpdate } = nonTvItemsToUpdate;
+
+        await Database.knex.transaction((trx) =>
+          mediaItemRepository.updateLastAndNextMediaItemAirings({
+            mediaItemIds: mediaItemsToUpdate,
+            trx,
+          })
+        );
+
+        logger.debug(
+          h`updated last and upcoming dates for ${mediaItemsToUpdate.length} mediaItems: ${mediaItemsToUpdate.join(', ')}`
+        );
+      }
+
+      await scheduleLastAndUpcomingAiringsUpdate();
+    },
   });
 };
